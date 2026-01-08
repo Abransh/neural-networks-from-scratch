bee02ec29160684193472f604304080bbb75ef45
abransh <abranshbaliyan2807@gmail.com>
2026-01-08 01:18:47 +0100

    inistalizing neuron with learning rate and epochs

--- 01_single_neuron.py
+++ 01_single_neuron.py
@@ -16,3 +16,24 @@ plt.xlabel("X")
 plt.ylabel("y")
 plt.title("Simple Data Set")
 plt.show()
+
+# here we have a simple data set and its plotted with some noise -1.5 to 1.5
+
+#now we move on to intializing the neuron 
+
+w= np.random.randn(1,1) # weight (1x1 matrix )
+b= np.random.randn(1,1) # bias (1x1 matrix)
+
+# hyperparameters (its like settings we choose)
+
+learning_rate = 0.01 # how big steps we take when learning
+epochs = 100 # how many times we want to run the loop or you can say how many times we want to look at all the data
+
+loss_history = [] # to store the loss at each epoch and plot later
+
+
+print(f"starting training..")
+print(f"intial wieghts: {w[0,0]:.4f}")
+print(f"intial bias: {b[0,0]:.4f}")
+print(f"Target is: w=2, b=1 (since our data is y = 2x + 1)")
+print("-" * 50)
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ so i am building first one neuron in 01_single_neuron.py to plot 100 numbers bet
 the equation is y = 2x + 1 + 0.1 + np.random.randn(100,1) * 0.5 
 or y = 2x + 1 + 0.1 + noise
 for noise we use randn because it gives us a bell curve of noise (most points close to the line, few far away) - more realistic
+
+
+for 2nd part we intialize the neuron with random weights and bias 
+define learning rate and epochs 
+define loss history
