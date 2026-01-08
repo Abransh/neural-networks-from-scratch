b7f9c35ae50c40393c52e7036819f42daba43aca
abransh <abranshbaliyan2807@gmail.com>
2026-01-08 01:07:24 +0100

    some edits

--- 01_single_neuron.py
+++ 01_single_neuron.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 np.random.seed(42) # if you know why i used 42 here, you are def goated hahahhaha 
 X = np.random.rand(100,1) * 10 # 100 random numbers between 0 and 10 , the argument for rand is (rows, columns)
 y = 2 * X + 1 + 0.1 + np.random.randn(100,1) * 0.5 # HERE we are adding some noise to the data because lets be real, perfection never comes handly or i would say doesnt exist 
+# randn gives us a bell curve of noise (most points close to the line, few far away) - more realistic
+
 
 # visualising the data 
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ so i am building first one neuron in 01_single_neuron.py to plot 100 numbers bet
 
 the equation is y = 2x + 1 + 0.1 + np.random.randn(100,1) * 0.5 
 or y = 2x + 1 + 0.1 + noise
+for noise we use randn because it gives us a bell curve of noise (most points close to the line, few far away) - more realistic
