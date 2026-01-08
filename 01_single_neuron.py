535c8e42e5d3085624d8fa87e760be7359704650
abransh <abranshbaliyan2807@gmail.com>
2026-01-08 01:05:57 +0100

    some edits

--- 01_single_neuron.py
+++ 01_single_neuron.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 # adding some simple data set poonts : y = 2x + 1 
 
 np.random.seed(42) # if you know why i used 42 here, you are def goated hahahhaha 
-X = np.random.rand(100,1) * 10 # 100 random numbers between 0 and 10 
-y = 2 * X + 1 + 0.1 + np.random.randn(100,1) * 0.5 
+X = np.random.rand(100,1) * 10 # 100 random numbers between 0 and 10 , the argument for rand is (rows, columns)
+y = 2 * X + 1 + 0.1 + np.random.randn(100,1) * 0.5 # HERE we are adding some noise to the data because lets be real, perfection never comes handly or i would say doesnt exist 
 
 # visualising the data 
 
