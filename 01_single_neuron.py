740e9c4f43c05682c5cb078a0fab63e1163144ad
abransh <abranshbaliyan2807@gmail.com>
2026-01-08 01:02:45 +0100

    some edits

--- 01_single_neuron.py
+++ 01_single_neuron.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # adding some simple data set poonts : y = 2x + 1 
 
-np.random.seed(42) # to get random numbers everytime 
+np.random.seed(42) # if you know why i used 42 here, you are def goated hahahhaha 
 X = np.random.rand(100,1) * 10 # 100 random numbers between 0 and 10 
 y = 2 * X + 1 + 0.1 + np.random.randn(100,1) * 0.5 
 
